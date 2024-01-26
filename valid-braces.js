ff67d5358387572daf019bad3c1a23cce826b08b
izael gomes <izaelgomes127@gmail.com>
2024-01-26 16:55:17 -0300

    first commit

---
+++ valid-braces.js
@@ -0,0 +1,32 @@
+function validBraces(braces){
+  const open = []
+  const caracters = braces.split("")
+  const mat = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  }
+
+  for (let i = 0; i < caracters.length; i++) {
+    if ([")", "]", "}"].includes(caracters[i]) && open.length <= 0) {
+      return false
+    }
+
+    if (["(", "[", "{"].includes(caracters[i])) {
+      open.push(caracters[i])
+      continue
+    }
+
+    if (!(open[open.length - 1] === mat[caracters[i]])) {
+      return false
+    }
+
+    open.pop()
+  }
+
+  return open.length === 0
+}
+
+console.log(validBraces("({})[({})]"))
+console.log(validBraces("(}"))
+console.log(validBraces("(((({{"))
